e84b399d5811801aac2da9329b4ac2bcdb00bbe1
RaccHoon <choco730522@gmail.com>
2024-01-18 02:38:38 +0900

    create(spring/order):스프링 예제 물건 주문 예제

--- src/main/java/hello/core/member/MemberRepository.java
+++ src/main/java/hello/core/member/MemberRepository.java
@@ -1,2 +1,8 @@
-package hello.core.member;public class MemberRepository {
+package hello.core.member;
+
+public interface MemberRepository {
+
+    void save(Member member);
+
+    Member findById(Long memberId);
 }
